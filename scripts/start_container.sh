5a5c97a42e5477ca445362cfd76dd9c2a0918173
arunmack789 <35661188+arunmack789@users.noreply.github.com>
2024-07-15 13:41:12 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull arunblaster789828/simple-python-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000 arunblaster789828/simple-python-app
+docker run -d -p 7000:7000 arunblaster789828/simple-python-app
 
